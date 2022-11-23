73b6e0a5be6373350b03787ef2fea8c061c4049e
Kamlesh Kumar Bind <kkb.mmmut@gmail.com>
2022-11-23 17:00:29 +0530

    this is the initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import './App.css'; 
+export default class rainyApp extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      rain:'false'
+    }
+  }
+  
+  render(){
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"> 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Is today is raining? {this.state.rain}</h1>
+        </p> 
       </header>
     </div>
+    </>
   );
+};
 }
 
-export default App;
+//export default rainyApp;
